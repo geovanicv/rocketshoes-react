037ca11247b1dd6dde36c1d724b20638053218cd
Geovani Cavalcante <geovani.scavalcante@gmail.com>
2020-01-13 13:39:12 -0300

    Configurando rotas

---
+++ modulo7-arquitetura-flux/src/pages/Cart/index.js
@@ -0,0 +1,8 @@
+// rfc
+import React from 'react';
+
+// import { Container } from './styles';
+
+export default function Cart() {
+  return <h1>Cart</h1>;
+}
---
+++ modulo7-arquitetura-flux/src/pages/Home/index.js
@@ -0,0 +1,8 @@
+// rfc
+import React from 'react';
+
+// import { Container } from './styles';
+
+export default function Home() {
+  return <h1>Home</h1>;
+}
