b35937666e0a23a9e952ac7432676cc9c9ae8879
Geovani Cavalcante <geovani.scavalcante@gmail.com>
2020-01-13 14:38:32 -0300

    Criando e estilizando Header

--- modulo7-arquitetura-flux/src/App.js
+++ modulo7-arquitetura-flux/src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import GlobalStyled from './styles/global';
 
+import GlobalStyled from './styles/global';
+import Header from './components/Header';
 import Routes from './routes';
 
 function App() {
   return (
     <BrowserRouter>
-      {/* <Header /> */}
+      <Header />
       <Routes />
       <GlobalStyled />
     </BrowserRouter>
