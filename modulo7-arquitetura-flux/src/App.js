f344b4ad4ba1903a77edf9436f1aa3c5a80178ff
Geovani Cavalcante <geovani.scavalcante@gmail.com>
2020-01-13 22:36:01 -0300

    Configurando o Redux

--- modulo7-arquitetura-flux/src/App.js
+++ modulo7-arquitetura-flux/src/App.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 import GlobalStyled from './styles/global';
 import Header from './components/Header';
 import Routes from './routes';
 
+import store from './store';
+
 function App() {
   return (
-    <BrowserRouter>
-      <Header />
-      <Routes />
-      <GlobalStyled />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Header />
+        <Routes />
+        <GlobalStyled />
+      </BrowserRouter>
+    </Provider>
   );
 }
 
